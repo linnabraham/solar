6f62db4b4114de0572459c1bdb8febea91d29280
linnabraham <linn.official@gmail.com>
2023-07-29 15:12:40 +0530

    Cleanup code

--- helpers/single_flare.py
+++ helpers/single_flare.py
@@ -11,6 +11,8 @@ import matplotlib.animation as animation
 from aiapy.calibrate import correct_degradation, normalize_exposure, register, update_pointing
 from aiapy.calibrate.util import get_correction_table, get_pointing_table
 import astropy.units as u
+import os
+from time import time
 
 class SolarFlare:
 
@@ -46,10 +48,8 @@ class SolarFlare:
         else:
             return results
 
-    def get_AIA(self, t_start: Timestamp , t_end: Timestamp, wavelength: int, email, exposure=2.0, download=False):
+    def get_AIA(self, t_start, t_end, wavelength: int, email, exposure=2.0, download=False):
 
-        t_start = t_start.to_pydatetime().isoformat()
-        t_end = t_end.to_pydatetime().isoformat()
         client = drms.Client(email=email)
         keys = ["EXPTIME", "QUALITY", "T_OBS", "T_REC", "WAVELNTH"]
         qstr = f"aia.lev1_euv_12s[{t_start}Z-{t_end}Z][? EXPTIME<{exposure} AND WAVELNTH={wavelength} ?]{{image}}"
@@ -58,37 +58,42 @@ class SolarFlare:
         records, filenames = client.query(qstr, key=keys, seg="image")
         print(f"{len(records)} records retrieved. \n")
         print(records)
-        #print(filenames)
-
 
         if download:
             export = client.export(qstr, method="url", protocol="fits")
-            import os
-            from time import time
-            dirname = int(time())
-            os.makedirs(f"data/{dirname}")
-            self.aia_files = export.download(f"data/{dirname}")
+            # create a unique dirname using the timestamp of download
+            dirname = f"data/{int(time())}"
+            os.makedirs(dirname)
+            print("Files are downloaded to :", dirname)
+            downloaded_files = export.download(dirname)
+            self.aia_files = {wavelength : downloaded_files}
+            return aia_files
         else:
             return records, filenames
 
     @staticmethod
-    def make_movie(fits_files: list, dest):
-        level_1_maps = sunpy.map.Map(fits_files)
+    def make_movie(fits_files: list, dest, lev_15=False):
 
-        pointing_table = get_pointing_table(level_1_maps[0].date - 3 * u.h, level_1_maps[-1].date + 3 * u.h)
-        correction_table = get_correction_table()
+        if lev_15:
+            level_1_maps = sunpy.map.Map(fits_files)
+                
+            pointing_table = get_pointing_table(level_1_maps[0].date - 3 * u.h, level_1_maps[-1].date + 3 * u.h)
+            correction_table = get_correction_table()
 
-        level_15_maps = []
+            level_15_maps = []
 
-        for a_map in level_1_maps:
-            map_updated_pointing = update_pointing(a_map, pointing_table=pointing_table)
-            map_registered = register(map_updated_pointing)
-            map_degradation = correct_degradation(map_registered, correction_table=correction_table)
-            map_normalized = normalize_exposure(map_degradation)
-            level_15_maps.append(map_normalized)
+            for a_map in level_1_maps:
+                map_updated_pointing = update_pointing(a_map, pointing_table=pointing_table)
+                map_registered = register(map_updated_pointing)
+                map_degradation = correct_degradation(map_registered, correction_table=correction_table)
+                map_normalized = normalize_exposure(map_degradation)
+                level_15_maps.append(map_normalized)
 
-        #sequence = sunpy.map.Map(fits_files, sequence=True)
-        sequence = sunpy.map.Map(level_15_maps, sequence=True)
+            sequence = sunpy.map.Map(level_15_maps, sequence=True)
+        else:
+
+            sequence = sunpy.map.Map(fits_files, sequence=True)
+
         ani = sequence.plot()
         Writer = animation.writers['ffmpeg']
         writer = Writer(fps=10, metadata=dict(artist='SunPy'), bitrate=1800)
