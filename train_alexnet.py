02d43367ff8f06aa0b37bf8b0d93006723cefc55
linnabraham <linn.official@gmail.com>
2023-10-05 21:54:14 +0530

    Added validation data, metrics and more callbacks

--- train_alexnet.py
+++ train_alexnet.py
@@ -5,10 +5,29 @@ import tensorflow as tf
 import numpy as np
 from helpers.alexnet import AlexNet
 from tensorflow.keras import backend
+from tensorflow.keras.callbacks import ModelCheckpoint, Callback, TensorBoard
 import wandb
 from wandb.keras import WandbCallback
+import sys
 
 wandb.init(project="AARP_Train")
+
+class SaveHistoryCallback(Callback):
+    def __init__(self, file_path):
+        super().__init__()
+        self.file_path = file_path
+        self.history = {'loss': [], 'val_loss': [], 'auc_pr':[], 'val_auc_pr':[], 'val_precision':[], 'val_recall':[]}
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.history['loss'].append(logs.get('loss'))
+        self.history['val_loss'].append(logs.get('val_loss'))
+        self.history['auc_pr'].append(logs.get('auc_pr'))
+        self.history['val_auc_pr'].append(logs.get('val_auc_pr'))
+        self.history['val_precision'].append(logs.get('val_precision'))
+        self.history['val_recall'].append(logs.get('val_recall'))
+
+        with open(self.file_path, 'w') as f:
+            json.dump(self.history, f)
 
 def read_fits(file_path):
     hdul = fits.open(file_path)
@@ -50,21 +69,55 @@ if __name__=="__main__":
                                             output_shapes = ())
 
     dataset = tf.data.Dataset.zip((images, labels))
-    dataset = dataset.batch(1)
 
 
     # force channels-first ordering
     backend.set_image_data_format('channels_first')
 
+    classification_threshold = 0.5
+
+    METRICS = [
+          tf.keras.metrics.Precision(thresholds=classification_threshold,
+                                     name='precision'),
+          tf.keras.metrics.Recall(thresholds=classification_threshold,
+                                  name="recall"),
+          tf.keras.metrics.AUC(num_thresholds=100, curve='PR', name='auc_pr'),
+    ]
+
+    pid = os.getpid()
+    output = "outputs"
+    outdir = os.path.join(output,str(pid))
+    os.makedirs(outdir)
+
+    model_path = os.path.join(output,"best_model.h5")
+    mc = ModelCheckpoint(model_path, monitor='val_loss', \
+            mode='min', verbose=1, save_best_only=True)
+
+    history_path = os.path.join(outdir,'history.json')
+    hc = SaveHistoryCallback(history_path)
+
+
+    # Define a function to accumulate the size of the dataset
+    def accumulate_size(count, _):
+            return count + 1
+
+    # Initialize a counter
+    initial_count = tf.constant(0)
+
+    # Use the reduce method to count the elements in the dataset
+    dataset_size = dataset.reduce(initial_count, accumulate_size)
+    dataset_size = dataset_size.numpy()
+    train_size = np.floor(0.8*dataset_size)
+    print("Train spit length", train_size)
+    train_data = dataset.take(train_size)
+    val_data = dataset.skip(train_size)
+    train_data = train_data.batch(8)
+    val_data = val_data.batch(8)
 
     with tf.device('/CPU:0'):
         model = AlexNet.build(width=512, height=512, depth=7, classes=1, reg=0.0002)
 
         print("[INFO] compiling model...")
-        model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3))
+        model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), metrics=METRICS)
 
-        history = model.fit(dataset, epochs=50, shuffle=True, callbacks=[WandbCallback()])
-        predictions = model.predict(dataset)
-        print(predictions)
-
-
+        history = model.fit(train_data, validation_data=val_data,  epochs=50, shuffle=True, callbacks=[mc,hc, WandbCallback()])
