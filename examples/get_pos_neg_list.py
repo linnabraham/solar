be64124f3fcba54d0441b074cfe4173362771915
linnabraham <linn.official@gmail.com>
2023-12-06 19:30:47 +0530

    Added code to remove near and off limb flares

--- examples/get_pos_neg_list.py
+++ examples/get_pos_neg_list.py
@@ -5,6 +5,7 @@ It takes the full list of AARP urls as input and assigns labels using the GOES e
 """
 import pandas as pd
 import sys,os
+import numpy as np
 
 def split_urllist(df, name):
 
@@ -30,6 +31,13 @@ def match_noaa_to_harpnum(x):
     else:
         return match[0]
 
+def remove_offlimb(goes_df):
+    # convert string field to tuple
+    goes_df['goes_location'] = goes_df.goes_location.apply(eval)
+    cutoff_meridian = 70
+    print("Removing near limb flares with meridian >", cutoff_meridian)
+    return goes_df[np.abs(goes_df.goes_location.apply(lambda x: x[0])) < cutoff_meridian ]
+
 if __name__ == "__main__":
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.abspath(os.path.join(cur_dir,".."))
@@ -40,6 +48,7 @@ if __name__ == "__main__":
     goes_df = pd.read_csv(os.path.join(parent_dir,"data/GOES_event_list.csv"))
     orginal_goes = goes_df.copy()
 
+    goes_df = remove_offlimb(goes_df)
     urldf = split_urllist(aarps_full_df,'urls')
     noaa_to_harps = split_ondupes(noaa_to_harps)
     flared_harps = pd.unique(noaa_to_harps.HARPNUM)
@@ -49,8 +58,9 @@ if __name__ == "__main__":
     main_class = goes_df['goes_class'].apply(lambda x: x[0])
     goes_df['harpnum'] =  goes_df['noaa_active_region'].apply(match_noaa_to_harpnum)
     urls_x = urldf[urldf['AARP'].isin(goes_df['harpnum'][main_class=='X'])]
+    print("Selecting only X class flares")
 
-    print("Writing to file -> list of urls for  all X-class flares")
+    print("Writing to file -> list of urls for flaring AARPS")
     urls_x.to_csv(os.path.join(parent_dir,f"data/aarps_pos_{n_urls_x}.csv"), index=False, header=None)
 
     # find number of flares to be selected from negative samples
@@ -58,13 +68,14 @@ if __name__ == "__main__":
     x_groups = grouped.ngroups
     n_urls_x = len(urls_x)
 
-    print("Number of X-class flare urls", n_urls_x)
-    print("X-class flare urls grouped by Wavelength", x_groups)
+    print("Number of X-class flare urls: ", n_urls_x)
+    print("X-class flare urls grouped by same AARP ID and Timestamp: ", x_groups)
 
     # select the first n non flaring aarps 
     all_neg = urldf[urldf['flared_labels']==0]
-    print("Number of non-flaring AR observations (urls)",len(all_neg))
+    print("Total number of non-flaring AR observations (urls): ",len(all_neg))
+    print("Non-flaring AARPS selected from the top of the list for balanced data: ", n_urls_x)
     balanced_neg = all_neg[:n_urls_x]
 
-    print("Writing to file -> list of urls for non-flaring ARs (balanced)")
+    print("Writing to file -> list of urls for non-flaring ARs")
     balanced_neg.urls.to_csv(os.path.join(parent_dir,f"data/aarps_neg_{n_urls_x}.csv"), index=False, header=None)
