e970ba01fecd011f7dffabd925e921c8e13ce60d
linnabraham <linn.official@gmail.com>
2024-05-04 09:47:27 +0530

    Add validation, savebestmodel callback and wandb integration

--- vit/torch_train.py
+++ vit/torch_train.py
@@ -11,6 +11,28 @@ import json
 from astropy.io import fits
 import time
 import argparse
+import os,sys
+import wandb
+import math
+
+class SaveBestModel:
+    def __init__(self, monitor='val_loss', mode='min'):
+        self.monitor = monitor
+        self.mode = mode
+        if mode == 'min':
+            self.best_value = float('inf')
+            self.monitor_op = lambda x, y: x < y
+        else:
+            self.best_value = float('-inf')
+            self.monitor_op = lambda x, y: x > y
+        
+    def __call__(self, val_metric, model, filepath):
+        if self.monitor_op(val_metric, self.best_value):
+            print(f"Validation {self.monitor}: {val_metric} improved from {self.best_value} to {val_metric}. Saving model...")
+            self.best_value = val_metric
+            torch.save(model.state_dict(), filepath)
+        else:
+            print(f"Validation {self.monitor}: {val_metric} did not improve from {self.best_value}.")
 
 class aia_euv(Dataset):
     def __init__(self, json_path, subset):
@@ -55,6 +77,16 @@ def train_loop():
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     threshold = 5000  # GPU memory threshold measured in megabytes
 
+    n_steps_per_epoch = math.ceil(len(train_loader.dataset) / args.batch_size)
+    print(f"Steps per epoch:{n_steps_per_epoch}")
+
+    wandb_dir = wandb.run.name
+    output_dir = os.path.join("output", wandb_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    save_best_model_callback = SaveBestModel(monitor='val_loss', mode='min')
+
     for epoch in range(args.epochs):
         model.train()
         running_loss = 0.0
@@ -83,12 +115,28 @@ def train_loop():
             loss.backward()
             optimizer.step()
             running_loss += loss.item() * inputs.size(0)
+            metrics = {"train/train_loss": loss,
+                       "train/epoch": (step + 1 + (n_steps_per_epoch * epoch)) / n_steps_per_epoch
+                       }
+            if step + 1 < n_steps_per_epoch:
+                # Log train metrics to wandb 
+                wandb.log(metrics)
+        val_loss, accuracy, precision, recall = validate_model(model, val_loader, criterion)
+
+        val_metrics = {"val/val_loss": val_loss, 
+                       "val/val_accuracy": accuracy,
+                       "val/precision":precision,
+                       "val/recall":recall}
+
+        wandb.log({**metrics, **val_metrics})
+
+        save_best_model_callback(val_loss, model, os.path.join(output_dir,"trained_model.pth"))
 
         epoch_loss = running_loss / len(train_dataset)
-        print(epoch_loss)
+        print(f"Epoch loss: {epoch_loss}")
         
         epoch_time = time.time() - start_time
-        print(f"Time taken to run single epoch: {epoch_time}")
+        print(f"Time taken to run single epoch: {epoch_time/60} mins")
 
         max_memory = torch.cuda.max_memory_allocated() / (1024 ** 2)  # Convert to megabytes
         print(f"Maximum GPU memory usage: {max_memory} MB")
@@ -96,12 +144,65 @@ def train_loop():
         max_memory_reserved = torch.cuda.max_memory_reserved() / (1024 ** 2)
         print(f"Maximum GPU memory reserved: {max_memory_reserved}")
 
+def validate_model(model, val_dl, loss_func):
+    model.eval()
+    val_loss = 0.
+    with torch.inference_mode():
+        correct = 0
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+        for i, (images, labels) in enumerate(val_dl):
+            images, labels = images.to(device), labels.to(device)
+
+            # Forward pass ➡
+            outputs = model(images)
+            val_loss += loss_func(outputs, labels)*labels.size(0)
+            # Compute accuracy and accumulate
+            pred_scores, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == labels).sum().item()
+
+            for pred,label in zip(predicted, labels):
+                if pred == 1 and label == 1:
+                    TP += 1
+                elif pred == 1 and label == 0:
+                    FP += 1
+                elif pred == 0 and label == 0:
+                    TN += 1
+                elif pred == 0 and label == 1:
+                    FN += 1
+
+        print("True Positives:", TP)
+        print("False Positives:", FP)
+        print("True Negatives:", TN)
+        print("False Negatives:", FN)
+        try:
+            precision = TP/(TP+FP)
+        except:
+            precision = 0
+        try:
+            recall = TP/(TP+FN)
+        except:
+            recall = 0
+    return val_loss / len(val_dl.dataset), correct / len(val_dl.dataset), precision, recall
+
 if __name__== "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-batch-size", "--batch-size", default=32)
     parser.add_argument("-epochs", "--epochs", default=5)
     parser.add_argument('-lr', '--lr', type=float, default=0.001)
     args = parser.parse_args()
+
+    args_dict = vars(args)
+
+    project_name = "flare_torch"
+
+    wandb.init(
+        project= project_name,
+        config= args_dict
+            )
 
     vit_model = DeepFlare_ViT(height=512, n_classes=2, n_passbands=7)
     model = vit_model.model
