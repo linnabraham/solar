041a1ad924308190955cafe7612226e24a70dae4
linnabraham <linn.official@gmail.com>
2024-06-08 19:45:14 +0530

    feat:weight pixel values to avoid border discontinuity

--- simple_padding.py
+++ simple_padding.py
@@ -11,6 +11,90 @@ import matplotlib
 import sunpy.visualization.colormaps as cm
 sdoaia304 = matplotlib.colormaps['sdoaia304']
 
+def pad_along_height(image, target_shape=(512,512)):
+    qs_pixels = list(image[:2, :].flatten()) + list(image[-2:,:].flatten())+ \
+        list(image[:,:2].flatten()) + list(image[:,-2:].flatten())
+    aspect_ratio = image.shape[1]/image.shape[0]
+    target_height, target_width = target_shape
+    height_before_pad = int(target_width/aspect_ratio)
+    #print("Height before pad", height_before_pad)
+    height_diff = target_height - height_before_pad
+    half_diff = height_diff // 2
+    #print("Half diff", half_diff)
+    black = np.zeros(target_shape)
+    target_width = target_shape[1]
+    resized_image = np.array(Image.fromarray(image).resize((target_width,
+                                                            height_before_pad)))
+    #print("shape of resized image", resized_image.shape)
+    if height_diff % 2 == 0:
+        black[half_diff:-half_diff,:] = resized_image
+    else:
+        black[half_diff:-(half_diff+1),:] = resized_image
+        black[-half_diff-1,:] = resized_image [-1,:]
+
+
+    black_top = black[:half_diff,:]
+    top_qs = np.random.choice(qs_pixels, size=(black_top.shape))
+    for row in np.arange(top_qs.shape[0]):
+        row_from_bottom = top_qs.shape[0]-row
+        top_qs_height = top_qs.shape[0]
+        x = row_from_bottom/top_qs_height
+        black_top[row_from_bottom-1, :] = top_qs[row_from_bottom-1,:] * (1-x) + \
+                resized_image[0,:] * x
+
+    black_bottom = black[-half_diff:,:] 
+    bottom_qs = np.random.choice(qs_pixels, size=(black_bottom.shape))
+    for row in np.arange(bottom_qs.shape[0]):
+        row_from_bottom = bottom_qs.shape[0]-row
+        bottom_qs_height = bottom_qs.shape[0]
+        x = row_from_bottom/bottom_qs_height
+        #black_bottom[-(row_from_bottom-1), :] = bottom_qs[-(row_from_bottom-1),:] * (1-x) + resized_image[-1:,:] * x
+        black_bottom[-(row_from_bottom), :] = bottom_qs[-(row_from_bottom),:] * (1-x) + resized_image[-1:,:] * x
+
+    return black
+
+def pad_along_width(image, target_shape=(512,512)):
+    qs_pixels = list(image[:2, :].flatten()) + list(image[-2:,:].flatten())+ \
+        list(image[:,:2].flatten()) + list(image[:,-2:].flatten())
+    aspect_ratio = image.shape[1]/image.shape[0]
+    target_height, target_width = target_shape
+    width_before_pad = int(target_height * aspect_ratio)
+    #print("Width before pad", width_before_pad)
+    width_diff = target_width - width_before_pad
+    half_diff = width_diff // 2
+    #print("Half diff", half_diff)
+    black = np.zeros(target_shape)
+    target_width = target_shape[1]
+    resized_image = np.array(Image.fromarray(image).resize((width_before_pad,
+                                                            target_height)))
+    #print("shape of resized image", resized_image.shape)
+    if width_diff % 2 == 0:
+        black[:,half_diff:-half_diff] = resized_image
+    else:
+        black[:,half_diff:-(half_diff+1)] = resized_image
+        black[:,-half_diff-1] = resized_image [:,-1]
+
+
+    black_left = black[:,:half_diff]
+    left_qs = np.random.choice(qs_pixels, size=(black_left.shape))
+    for col in np.arange(left_qs.shape[1]):
+        col_from_right = left_qs.shape[1]-col
+        left_qs_width = left_qs.shape[1]
+        x = col_from_right/left_qs_width
+        black_left[:,col_from_right-1] = left_qs[:,col_from_right-1] * (1-x) + \
+                resized_image[:,0] * x
+
+    black_right = black[:,-half_diff:]
+    right_qs = np.random.choice(qs_pixels, size=(black_right.shape))
+    for col in np.arange(right_qs.shape[1]):
+        col_from_right = right_qs.shape[1]-col
+        right_qs_width = right_qs.shape[1]
+        x = col_from_right/right_qs_width
+        black_right[:,-col_from_right] = right_qs[:,-col_from_right] * (1-x) + \
+                resized_image[:,-3] * x
+
+    return black
+
 def downscale_and_pad(image, target_shape=(512,512)):
     """
     Function to downsize image to specified size
@@ -25,44 +109,10 @@ def downscale_and_pad(image, target_shape=(512,512)):
     aspect_ratio = image.shape[1]/image.shape[0]
     #assert aspect_ratio != 1.
     if aspect_ratio > 1:
-        padded_height = int(target_width/aspect_ratio)
-        img_to_paste = np.array(Image.fromarray(image).resize((target_width, padded_height)))
-        height_diff = target_height - padded_height
-        #assert height_diff%2 == 0
-        qs_pixels = list(image[:2, :].flatten()) + list(image[-2:,:].flatten())+ \
-                list(image[:,:2].flatten()) + list(image[:,-2:].flatten())
-        black = np.zeros((target_height, target_width))
-        half_diff = int(height_diff/2)
-        black[:half_diff,:] = np.random.choice(qs_pixels, size=(black[:half_diff,:].shape))
-        if height_diff % 2 == 1:
-            org_height = img_to_paste.shape[0]
-            black[half_diff:-half_diff-1,:] = img_to_paste
-            black[-half_diff-1:,:] = np.random.choice(qs_pixels, size=(black[-half_diff-1:,:].shape))
-        else:
-
-            black[half_diff:-half_diff,:] = img_to_paste
-            black[-half_diff:,:] = np.random.choice(qs_pixels, size=(black[-half_diff:,:].shape))
-        return black
+        return pad_along_height(image, target_shape=target_shape)
 
     elif aspect_ratio < 1:
-        padded_width = int(target_height * aspect_ratio)
-        img_to_paste = np.array(Image.fromarray(image).resize((padded_width, target_height)))
-        width_diff =  target_width - padded_width
-        #assert width_diff%2 == 0
-        qs_pixels = list(image[:2, :].flatten()) + list(image[-2:,:].flatten())+ \
-                list(image[:,:2].flatten()) + list(image[:,-2:].flatten())
-        black = np.zeros((target_height, target_width))
-        half_diff = int(width_diff/2)
-        black[:, :half_diff] = np.random.choice(qs_pixels, size=(black[:, :half_diff].shape))
-
-        if width_diff%2 == 1:
-            org_width = img_to_paste.shape[1]
-            black[:, half_diff:-half_diff-1] = img_to_paste
-            black[:, -half_diff-1:] = np.random.choice(qs_pixels, size=(black[:, -half_diff-1:].shape))
-        else:
-            black[:, half_diff:-half_diff] = img_to_paste
-            black[:, -half_diff:] = np.random.choice(qs_pixels, size=(black[:, -half_diff:].shape))
-        return black
+        return pad_along_width(image, target_shape=target_shape)
 
     else:
         resized = np.array(Image.fromarray(image).resize((target_width, target_height)))
