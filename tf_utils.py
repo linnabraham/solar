fac5fc3e688095495623abf4db8af796f0874285
linnabraham <linn.official@gmail.com>
2024-06-18 15:12:43 +0530

    Add function to get compiled model

--- tf_utils.py
+++ tf_utils.py
@@ -1,4 +1,6 @@
 import argparse
+import tensorflow as tf
+from tensorflow.keras import backend
 from helpers.alexnet import AlexNet
 
 def get_trained_model(args):
@@ -15,3 +17,23 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-input-shape', '--input-shape', nargs='+', type=int, default=(512,512))
     return parser
+
+def get_compiled_model(args):
+   # force channels-first ordering
+    backend.set_image_data_format('channels_first')
+
+    classification_threshold = 0.5
+
+    METRICS = [
+          tf.keras.metrics.Precision(thresholds=classification_threshold,
+                                     name='precision'),
+          tf.keras.metrics.Recall(thresholds=classification_threshold,
+                                  name="recall"),
+          tf.keras.metrics.AUC(num_thresholds=100, curve='PR', name='auc_pr'),
+    ]
+    model = AlexNet.build(width=width, height=height, depth=7, classes=1, reg=0.0002)
+
+    print("[INFO] compiling model...")
+    model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), metrics=METRICS)
+    return model
+
