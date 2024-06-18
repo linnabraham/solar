731eb7cc691b560accdd70a13ad0316cc84b0527
linnabraham <linn.official@gmail.com>
2024-06-18 16:46:15 +0530

    Add code for dataset generation

--- tf_utils.py
+++ tf_utils.py
@@ -1,5 +1,9 @@
 import argparse
 import tensorflow as tf
+import os
+import json
+import numpy as np
+from astropy.io import fits
 from helpers.alexnet import AlexNet
 
 def get_trained_model(args):
@@ -32,4 +36,62 @@ def get_compiled_model(args):
     print("[INFO] compiling model...")
     model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), metrics=METRICS)
     return model
+
+def parse_images(imgs:list, args):
+    height, width = args.input_shape
+    images = np.zeros((len(imgs), height, width))
+    for i, img in enumerate(imgs):
+        hdul = fits.open(img)
+        image = hdul[0].data
+        images[i,:,:] = image
+    return images
+
+def img_generator(collection, args):
+    for element in collection:
+        yield parse_images(element, args)
+
+def label_generator(collection):
+    for element in collection:
+        yield element
+
+def parse_json(json_path):
+    with open(json_path) as f:
+        data = json.load(f)
+
+        x_train = [
+                [os.path.join(os.path.dirname(json_path), c[str(i)]) for i in range(7)]
+                 for c in data.get('training')
+                 ]
+        y_train = [p['label'] for p in data.get('training')]
+
+        x_val  = [
+                [os.path.join(os.path.dirname(json_path), c[str(i)]) for i in range(7)]
+                 for c in data.get('validation')
+                 ]
+        y_val = [p['label'] for p in data.get('validation')]
+    return x_train, y_train, x_val, y_val
+
+def dataset_from_json(args):
+    height, width = args.input_shape
+    nchannels = args.num_channels
+    x_train, y_train, x_val, y_val = parse_json(args.json_path)
+
+    images = tf.data.Dataset.from_generator(generator = lambda: img_generator(x_train, args),
+                                            output_types=tf.float32,
+                                            output_shapes=[nchannels, height, width])
+    labels = tf.data.Dataset.from_generator(generator = lambda: label_generator(y_train),
+                                            output_types = tf.int32,
+                                            output_shapes = ())
+
+    train_ds = tf.data.Dataset.zip((images, labels))
+
+    images = tf.data.Dataset.from_generator(generator = lambda: img_generator(x_val, args),
+                                            output_types=tf.float32,
+                                            output_shapes=[nchannels, height, width])
+    labels = tf.data.Dataset.from_generator(generator = lambda: label_generator(y_val),
+                                            output_types = tf.int32,
+                                            output_shapes = ())
+    val_ds = tf.data.Dataset.zip((images, labels))
+
+    return train_ds, val_ds
 
