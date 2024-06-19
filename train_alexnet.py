6fb66f4cd2a9510019d3fca44d41dcc084ee3f1c
linnabraham <linn.official@gmail.com>
2024-06-19 15:45:10 +0530

    Prevent tensorflow from reserving the entire GPU memory

--- train_alexnet.py
+++ train_alexnet.py
@@ -142,6 +142,8 @@ def save_arguments(args, filename):
 
 if __name__=="__main__":
     from wandb.keras import WandbCallback
+    gpu = tf.config.experimental.list_physical_devices('GPU')[0]
+    tf.config.experimental.set_memory_growth(gpu, True)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-input-shape', '--input-shape', nargs='+', type=int, default=(512,512))
