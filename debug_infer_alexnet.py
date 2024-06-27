597eb5c0f5932770966ce21b90c69c9699bd29a0
linnabraham <linn.official@gmail.com>
2024-06-27 08:09:27 +0530

    Clean up the code

--- debug_infer_alexnet.py
+++ debug_infer_alexnet.py
@@ -3,46 +3,9 @@ import tensorflow as tf
 from sklearn.utils import shuffle
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
 import numpy as np
-from tf_utils import get_trained_model, get_parser, dataset_from_json, parse_json, parse_images, read_stats
+from tf_utils import get_trained_model, get_parser, parse_json, parse_images
 from helpers.alexnet import AlexNet
-from train_alexnet import add_normalization_layer
-
-def preprocess_images_E2(images):
-    images = np.where(images<0, np.zeros_like(images), images)
-    images = np.sqrt(images)
-    return images
-
-def preprocess_images_E6(images):
-    images = np.where(images<0, np.zeros_like(images), images)+1
-    images = np.log(images)
-    return images
-
-def preprocess_label_E2(label):
-    return int(label)
-
-def per_image_standardization(image):
-    mean = np.mean(image)
-    stddev = np.std(image)
-    # Prevent division by zero
-    return standardized_image
-
-def evaluate_on_tf_datasets(args):
-    train_ds, val_ds = dataset_from_json(args)
-    train_ds = train_ds.shuffle(buffer_size=1000).batch(args.batch_size)
-    train_ds = train_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-
-    print("Iterating through dataset")
-    for images_batch, labels_batch in train_ds:
-        label = labels_batch[0].numpy()
-        if label == 0:
-            count += 1
-        else:
-            continue
-        if count > 10:
-            break
-        prediction = model.predict(images_batch)
-        print("Labels\n", label)
-        print(prediction)
+from train_alexnet import get_compiled_model
 
 def evaluate_model_metrics(x_train, y_train):
     count = 0
@@ -52,11 +15,8 @@ def evaluate_model_metrics(x_train, y_train):
         if count > 50:
             break
         print("Label", label)
-        label = preprocess_label_E2(label)
         ground_truth.append(label)
         images = parse_images(image_paths, args)
-        images = preprocess_images_E2(images)
-        images = per_image_standardization(images)
         images = np.expand_dims(images, axis=0)
         prediction = model.predict(images)
         pred_scores.append(prediction[0])
@@ -82,39 +42,35 @@ def print_weights(model):
     print(second_last_layer_weights)
     print(last_layer_weights)
 
-def build_model_withnorm(args):
+def plot_hist(model):
+    first_layer_weights = model.trainable_variables[0]
+    sliced_weights = first_layer_weights[:,:,0,0]
+    plt.hist(sliced_weights.numpy().flatten(), bins=50)
+    #plt.hist(sliced_weights.numpy().flatten(), range=(-0.05,0.05), bins=50)
+    plt.show()
 
-    height, width = args.input_shape
-    classification_threshold = 0.5
-    model = AlexNet.build(width=width, height=height, depth=7, classes=1, reg=0.0002)
-    data_mean, data_std = read_stats('/data/linn/stats.pkl')
-    model = add_normalization_layer(model, data_mean = data_mean, data_std = data_std, args=args)
-    #data_var = [np.square(item) for item in data_std]
-    ##norm_layer = tf.keras.layers.Normalization(axis=-1, mean=data_mean, variance=data_var)
-    #norm_layer = tf.keras.layers.Normalization(axis=1, mean=data_mean, variance=data_var)
-    #inputs = tf.keras.Input(shape=(args.num_channels,)+args.input_shape)
-    ##inputs = tf.keras.Input(shape=args.input_shape+(args.num_channels,))
-    #x = norm_layer(inputs)
-    #outputs = model(x)
-    #model = tf.keras.Model(inputs, outputs)
-    return model
 
 if __name__=="__main__":
     # force channels-first ordering
     from tensorflow.keras import backend
+    import matplotlib.pyplot as plt
     backend.set_image_data_format('channels_first')
     gpu = tf.config.experimental.list_physical_devices('GPU')[0]
     tf.config.experimental.set_memory_growth(gpu, True)
 
     parser = get_parser()
-    parser.add_argument("--json-path")
-    parser.add_argument("--modelpath")
+    parser.add_argument("--json-path", help="json file with training data paths")
+    parser.add_argument("--modelpath", help="path of trained model")
     parser.add_argument("--batch-size", type=int, default=1)
+    parser.add_argument("--stats-file", help="path of pickle file containing data mean and std")
     args = parser.parse_args()
 
-    model = get_trained_model(args)
-    model = build_model_withnorm(args)
+    model = get_compiled_model(args)
+    model.load_weights(args.modelpath)
     print_weights(model)
+    plot_hist(model)
+    plt.savefig("hist_trained_weights.png")
+    plt.close()
 
     if not args.json_path is None:
         x_train, y_train, x_test, y_test = parse_json(args.json_path)
@@ -123,4 +79,3 @@ if __name__=="__main__":
         print(len(x_train), len(x_test))
 
         evaluate_model_metrics(x_train, y_train)
-
