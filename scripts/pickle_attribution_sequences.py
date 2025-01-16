58127c3d2cc7ad77f5d38335f4ddafc5b353403a
linnabraham <linn.official@gmail.com>
2025-01-16 11:10:02 +0530

    Update script

--- scripts/pickle_attribution_sequences.py
+++ scripts/pickle_attribution_sequences.py
@@ -4,8 +4,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pickle
 import argparse
 from aarp_ml.model import training
-from aarp_ml.integrated_gradients import aarp_ig
+from aarp_ml.integrated_gradients.aarp_ig import get_attribution_sequence
 from aarp_ml.dataset import aarp_dataset
+from aarp_ml.integrated_gradients.aarp_ig import aarp_intensities_with_attribution
 """
 Create attribution sequence for all AARPs in a given subset
 using a given model and pickle objects to disk inside a directory
@@ -14,32 +15,35 @@ named with the model code name
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--trained_model_path', required=True)
+    parser.add_argument('--trained_model_path', required=False)
     args = parser.parse_args()
 
     input_shape=(512,512)
     num_channels=7
-    #trained_model_path = "outputs/electric-star-195/best_model.h5",
+    if args.trained_model_path is None:
+        args.trained_model_path = "outputs/electric-star-195/best_model.h5"
+    print(f"Using trained model:{args.trained_model_path}")
+
     stats_file=os.path.expanduser("~/july/solar/stats.pkl")
     dataset = aarp_dataset(json_path=os.path.expanduser("~/july/solar/solar_dataset.json"))
     test_ds = dataset.get_subset('test')
 
-    alexnet = training(aarp_dataset, stats_file, input_shape, num_channels)
-    model = alexnet.get_trained_model(
-            trained_model_path = args.trained_model_path,
-            ).model
 
     unique_id = args.trained_model_path.split("/")[1]
-    # dest_dir = f"/data/linn/attributions-{unique_id}"
-    dest_dir = f"attributions-{unique_id}"
-    print("Creating directory", dest_dir)
-    os.mkdir(dest_dir)
+    dest_dir = f"/data/linn/intensities_with_attributions-{unique_id}"
+    if not os.path.exists(dest_dir):
+        print("Creating directory", dest_dir)
+        os.mkdir(dest_dir)
+    else:
+        raise ValueError(f"Directory {dest_dir} already exists")
 
-    ig = aarp_ig(input_shape, num_channels, model)
+    model = training(dataset, stats_file).get_trained_model(args.trained_model_path).model
     for aarp_id in test_ds.unique_aarp_ids:
         print("Processing AARP ID:", aarp_id)
         aarp_seq = test_ds.create_aarp_sequence(aarp_id=aarp_id)
-        attribution_seq = ig.get_attribution_sequence(aarp_seq)
+        attribution_seq = get_attribution_sequence(aarp_seq, model)
+        aarp_int_with_attbn = aarp_intensities_with_attribution(
+                aarp_seq, attribution_seq)
         print("Pickling output to disk...")
-        with open(f'{dest_dir}/attribution_seq_{aarp_id}.pkl', 'wb') as file:
-            pickle.dump(attribution_seq, file)
+        with open(f'{dest_dir}/intensities_with_attribution_seq_{aarp_id}.pkl', 'wb') as file:
+            pickle.dump(aarp_int_with_attbn, file)
