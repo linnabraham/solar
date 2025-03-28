3521d38df30e43edd108980302cbbbe5b3fb9871
linnabraham <linn.official@gmail.com>
2025-03-28 05:57:19 +0530

    Clean up code

--- vit/scripts/train.py
+++ vit/scripts/train.py
@@ -11,20 +11,40 @@ import pickle
 from torchvision.transforms import v2
 import torch
 from torch.utils.data import DataLoader, WeightedRandomSampler
-import json
 from collections import Counter
+from sklearn.metrics import confusion_matrix
 
 threshold = 5000  # GPU memory threshold measured in megabytes
+
+def get_weighted_sampler(dataset):
+    """
+    Create a WeightedRandomSampler for handling class imbalance.
+
+    Args:
+        dataset: A dataset object with a `labels` attribute containing class labels.
+
+    Returns:
+        WeightedRandomSampler: A sampler that samples based on class distribution.
+    """
+    # Count occurrences of each class in the dataset
+    class_counts = Counter(dataset.labels)
+    total_samples = sum(class_counts.values())
+
+    # Compute class weights (inverse of frequency)
+    class_weights = {cls: total_samples / count for cls, count in class_counts.items()}
+
+    # Assign a weight to each sample based on its class
+    sample_weights = [class_weights[label] for label in dataset.labels]
+
+    return WeightedRandomSampler(weights=sample_weights, num_samples=len(dataset), replacement=True)
 
 def validate_model(model, val_dl, loss_func, device):
     model.eval()
     val_loss = 0.
+    y_true, y_pred = [], []
+
     with torch.inference_mode():
-        correct = 0
-        TP = 0
-        FP = 0
-        TN = 0
-        FN = 0
+
         for i, (images, labels) in tqdm(enumerate(val_dl), total=len(val_dl), leave=False):
             images, labels = images.to(device), labels.to(device)
 
@@ -33,31 +53,19 @@ def validate_model(model, val_dl, loss_func, device):
             val_loss += loss_func(outputs, labels)*labels.size(0)
             # Compute accuracy and accumulate
             pred_scores, predicted = torch.max(outputs.data, 1)
-            correct += (predicted == labels).sum().item()
+            
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(predicted.cpu().numpy())
 
-            for pred,label in zip(predicted, labels):
-                if pred == 1 and label == 1:
-                    TP += 1
-                elif pred == 1 and label == 0:
-                    FP += 1
-                elif pred == 0 and label == 0:
-                    TN += 1
-                elif pred == 0 and label == 1:
-                    FN += 1
-
+        TN, FP, FN, TP = confusion_matrix(y_true, y_pred).ravel()
+        precision = TP/(TP+FP) if (TP+FP) > 0 else 0
+        recall = TP/(TP+FN) if (TP+FN) > 0 else 0
         print("True Positives:", TP)
         print("False Positives:", FP)
         print("True Negatives:", TN)
         print("False Negatives:", FN)
-        try:
-            precision = TP/(TP+FP)
-        except:
-            precision = 0
-        try:
-            recall = TP/(TP+FN)
-        except:
-            recall = 0
-    return val_loss / len(val_dl.dataset), correct / len(val_dl.dataset), precision, recall
+
+    return val_loss / len(val_dl.dataset), (TP+TN) / len(val_dl.dataset), precision, recall
 
 def training_step(inputs, labels, model, criterion, optimizer):
     optimizer.zero_grad()
@@ -135,11 +143,6 @@ def train_loop(train_loader, val_loader, model, device, output_dir, args):
         epoch_loss = running_loss / len(train_loader.dataset)
         print(f"Epoch loss: {epoch_loss}")
 
-        val_ds = aia_euv(args.json_path, subset='validation')
-
-        ig_val_loader = DataLoader(val_ds, batch_size = 64, shuffle=True)
-        #log_ig_attributes(model, ig_val_loader, batch_idx=0, channel=0)
-
         epoch_time = time.time() - start_time
         print(f"Time taken to run single epoch: {epoch_time/60} mins")
 
@@ -175,23 +178,6 @@ def train(args):
         v2.RandomVerticalFlip(p=0.5)
         ]))
 
-    # Compute class weights
-    class_counts = Counter(train_dataset.labels)  # Count occurrences
-    total_samples = sum(class_counts.values())
-
-    # Compute weights for each class (inverse of frequency)
-    class_weights = {cls: total_samples / count for cls, count in class_counts.items()}
-
-    # Assign sample weights
-    sample_weights = [class_weights[label] for label in train_dataset.labels]
-
-    # Create a sampler
-    sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(train_dataset), replacement=True)
-
-    # Create DataLoader with oversampling
-    train_loader = DataLoader(train_dataset, batch_size=32, sampler=sampler)
-
-
     validation_dataset = aia_euv(json_path, subset='validation', transform=v2.Compose([CustomTransform(means, stds)]))
 
     print("Checking data specifications")
@@ -203,10 +189,9 @@ def train(args):
     print("train size", len(train_dataset))
     print("validation size", len(validation_dataset))
 
-    train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True)
-
+    # Create DataLoader with oversampling
+    train_loader = DataLoader(train_dataset, batch_size=32, sampler=get_weighted_sampler(train_dataset))
     val_loader = DataLoader(validation_dataset, batch_size = args.batch_size, shuffle=True)
-
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Using device {device}")
 
@@ -219,7 +204,7 @@ def train(args):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    train_loop(train_dataset, train_loader, val_loader, model, device, output_dir, args)
+    train_loop(train_loader, val_loader, model, device, output_dir, args)
 
 if __name__== "__main__":
 
