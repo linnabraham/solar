84706e3a14e0f1a21b7ce0a8f5bec3c43316a973
linnabraham <linn.official@gmail.com>
2025-04-06 05:18:59 +0530

    feat: Log epoch and step separately

--- vit/scripts/train.py
+++ vit/scripts/train.py
@@ -129,7 +129,8 @@ def train_loop(train_loader, val_loader, model, device, output_dir, args):
             loss = training_step(inputs, labels, model, criterion, optimizer)
             running_loss += loss.item() * inputs.size(0)
             metrics = {"train/train_loss": loss,
-                       "train/epoch": (step + 1 + (n_steps_per_epoch * epoch)) / n_steps_per_epoch
+                       "train/step": (step + 1 + (n_steps_per_epoch * epoch)) / n_steps_per_epoch,
+                       "train/epoch": epoch + 1,
                        }
             if step + 1 < n_steps_per_epoch:
                 # Log train metrics to wandb
