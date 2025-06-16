9d6bc4bf0e22f961519ec4290ed557a97b2e624a
linnabraham <linn.official@gmail.com>
2025-06-16 12:00:42 +0530

    Feat: Add timestamp property to single_aarp class

--- vit/scripts/ig.py
+++ vit/scripts/ig.py
@@ -20,6 +20,10 @@ class single_aarp:
         self.aarp_id = aarp_id
         self.dataframe = dataframe
 
+    @property
+    def timestamps(self):
+        return self.dataframe.timestamp
+
     def get_images(self):
         all_filepaths = self.dataframe[all_wavelengths].values.tolist()
         images = np.zeros(shape=(len(all_filepaths), 7, 512, 512))
