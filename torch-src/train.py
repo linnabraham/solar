63cd9974c4c78c791be680b8804afd4998f7c376
linnabraham <linn.official@gmail.com>
2025-06-26 17:13:12 +0530

    Fix: Train for 250 epochs

--- torch-src/train.py
+++ torch-src/train.py
@@ -51,7 +51,7 @@ class TrainingConfig:
 
     # Optional training parameters
     batch_size: int = 32
-    epochs: int = 5
+    epochs: int = 250
     learning_rate: float = 0.001
     scheduler_type: Optional[str] = None
     retrain: bool = False
