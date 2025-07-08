07434eb1373ea86987a39ab1fd3bc6f7fc90ef6c
linnabraham <linn.official@gmail.com>
2025-07-08 16:32:45 +0530

    Feat: Combine different feature sets to single feature set for xgb_train; Add function to generate feature names

--- src/torch/xgb_train.py
+++ src/torch/xgb_train.py
@@ -6,8 +6,8 @@ from typing import Optional, List
 from dataclasses import dataclass
 import numpy as np
 from sklearn.metrics import log_loss
-from vit.scripts.train import print_config
-from torch_src.train import init_data
+from src.torch.vit.train import print_config
+from src.torch.train import init_data
 import xgboost as xgb
 import numpy as np
 from sklearn.metrics import log_loss
@@ -22,7 +22,6 @@ class TrainingConfig:
     # Optional training parameters
     batch_size: int = 32
     retrain: bool = False
-    feature_type: str = "simple"
     trained_model_path: Optional[str] = None
 
     # Model parameters
@@ -33,29 +32,12 @@ class TrainingConfig:
     # System parameters
     device: str = "cuda:0"
     memory_threshold: int = 5000  # GPU memory threshold measured in megabytes
+    simple_stats: tuple = ("min", "max", "mean")
+    percentiles: tuple = (60, 80, 90, 95, 98, 99)
+    passbands: tuple = ('94', '131', '171', '193', '211', '304', '335')
 
 # Assuming you have a TensorFlow or Torch generator that yields (images, labels)
-def extract_simple_stats_generator(dataset):
-    features = []
-    labels = []
-    dataset_iter = iter(dataset)
-    for images, label in tqdm(dataset_iter):
-        # Assuming images shape is (batch_size, height, width, channels)
-        images, label = images.to(config.device), label.to(config.device)
-        for i in range(images.shape[0]):
-            image = images[i]
-            # Calculate min, max, mean for each channel
-            channel_stats = []
-            for channel in range(image.shape[0]):
-                channel_data = image[channel, :, :]
-                channel_stats.extend([torch.min(channel_data).item(), torch.max(channel_data).item(), torch.mean(channel_data).item()])
-
-            features.append(channel_stats)
-            labels.append(label[0].item())  # Assuming label is a single value for the image
-
-    return np.array(features), np.array(labels)
-
-def extract_percentiles_generator(dataset):
+def extract_stats_generator(dataset, config:TrainingConfig):
     features = []
     labels = []
     dataset_iter = iter(dataset)
@@ -64,17 +46,27 @@ def extract_percentiles_generator(dataset):
         # We take the first image in the batch
         image = images[0].numpy()
 
-        # Calculate fixed percentile values for each channel
+        # Calculate basic stats and fixed percentile values for each channel
         channel_stats = []
+
         for channel in range(image.shape[0]):
             channel_data = image[channel, :, :]
-            channel_stats.extend([np.percentile(channel_data,60), np.percentile(channel_data,80), np.percentile(channel_data,90),
-                                  np.percentile(channel_data,95), np.percentile(channel_data,98), np.percentile(channel_data,99)])
+            percentile_feats = [np.percentile(channel_data,percentile) for percentile in config.percentiles]
+            channel_data = torch.from_numpy(channel_data)
+            channel_stats.extend([torch.min(channel_data).item(), torch.max(channel_data).item(), torch.mean(channel_data).item()])
+            channel_stats.extend(percentile_feats)
 
         features.append(channel_stats)
         labels.append(label[0])  # Assuming label is a single value for the image
 
     return np.array(features), np.array(labels)
+
+def get_feature_names(config:TrainingConfig):
+    stat_names = list(config.simple_stats)
+    percentile_names = [f'p{percentile}' for percentile in config.percentiles]
+    stat_names.extend(percentile_names)
+    feat_names = [f'{pb}_{stat_name}' for pb in config.passbands for stat_name in stat_names]
+    return feat_names
 
 def train_and_eval(config: TrainingConfig):
     """Main training function."""
@@ -90,14 +82,8 @@ def train_and_eval(config: TrainingConfig):
     output_dir = os.path.join("output", wandb.run.name)
     os.makedirs(output_dir, exist_ok=True)
 
-    if config.feature_type == "simple":
-        X_train, y_train = extract_simple_stats_generator(train_loader)
-        X_val, y_val = extract_simple_stats_generator(val_loader)
-    elif config.feature_type == "percentile":
-        X_train, y_train = extract_percentiles_generator(train_loader)
-        X_val, y_val = extract_percentiles_generator(val_loader)
-    else:
-        raise ValueError("Method not implemented")
+    X_train, y_train = extract_stats_generator(train_loader, config)
+    X_val, y_val = extract_stats_generator(val_loader, config)
 
     # Convert the dataset into DMatrix format for XGBoost
     dtrain = xgb.DMatrix(X_train, label=y_train)
